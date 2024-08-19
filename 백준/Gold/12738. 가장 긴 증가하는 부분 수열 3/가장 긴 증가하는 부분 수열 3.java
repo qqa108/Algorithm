788e221b99ce6950df81f0771c044cf984d2a3fe
Choi yonghun <52096623+qqa108@users.noreply.github.com>
2024-08-19 15:13:02 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 536 ms, Memory: 170196 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int arr[];
+	static int dp[];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		arr = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		dp = new int[N + 1];
+		Arrays.fill(dp, -Integer.MAX_VALUE);
+		int index = 1;
+		for (int i = 1; i <= N; i++) {
+			if (dp[index - 1] < arr[i]) {
+				dp[index++] = arr[i];
+			} else if (dp[index - 1] > arr[i]) {
+				dp[binary(0, index - 1, arr[i])] = arr[i];
+			}
+		}
+
+		int count = 0;
+		for (int i = 0; i <= N; i++) {
+			if (dp[i] != -Integer.MAX_VALUE) {
+				count++;
+			}
+		}
+
+		System.out.println(count);
+	}
+
+	static int binary(int left, int right, int target) {
+		int mid = 0;
+
+		while (true) {
+			if (left >= right) {
+				break;
+			}
+			mid = (left + right) / 2;
+			if (dp[mid] < target) {
+				left = mid + 1;
+
+			} else {
+				right = mid;
+			}
+		}
+		return right;
+	}
+}
