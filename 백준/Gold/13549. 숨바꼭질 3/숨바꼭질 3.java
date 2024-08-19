5117f084836b78f655821854c6868ffbf3d57d2e
Choi yonghun <52096623+qqa108@users.noreply.github.com>
2024-08-19 15:15:01 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 144 ms, Memory: 16492 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,63 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	static int N;
+	static int M;
+	static boolean visit[];
+	static int delta[] = { N, -1, +1 };
+	static int answer = Integer.MAX_VALUE;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		visit = new boolean[100001];
+		bfs();
+
+		System.out.println(answer);
+
+		sc.close();
+	}
+
+	static class Num {
+		int num;
+		int count;
+
+		public Num(int num, int count) {
+			this.num = num;
+			this.count = count;
+		}
+	}
+
+	static void bfs() {
+		Queue<Num> queue = new ArrayDeque<>();
+		queue.add(new Num(N, 0));
+		visit[N] = true;
+
+		while (true) {
+			if (queue.isEmpty()) {
+				break;
+			}
+
+			Num now = queue.poll();
+			if (now.num == M) {
+				answer = Math.min(answer, now.count);
+				continue;
+			}
+			delta[0] = now.num;
+			for (int i = 0; i < 3; i++) {
+				if (now.num + delta[i] >= 0 && now.num + delta[i] <= 100000 && !visit[now.num + delta[i]]) {
+					visit[now.num + delta[i]] = true;
+					if (i == 0) {
+						queue.add(new Num(now.num + delta[i], now.count));
+					} else {
+						queue.add(new Num(now.num + delta[i], now.count + 1));
+					}
+
+				}
+			}
+		}
+	}
+}
