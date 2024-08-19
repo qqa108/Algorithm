de843f3c0f7560a4e209b6fb050c4122d347e3ba
Choi yonghun <52096623+qqa108@users.noreply.github.com>
2024-08-19 14:46:26 +0900

    [Gold V] Title: 동전 1, Time: 88 ms, Memory: 11744 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int arr[] = new int[N];
+		int dp[] = new int[K + 1];
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		dp[0] = 1;
+		for (int i = 0; i < N; i++) {
+			for (int j = 1; j <= K; j++) {
+				if (j - arr[i] >= 0) {
+					dp[j] = dp[j] + dp[j - arr[i]];
+				}
+			}
+		}
+		System.out.println(dp[K]);
+	}
+}
