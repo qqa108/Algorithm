a33bb43c0b93938e72227f7b415378789bbd3e63
Choi yonghun <52096623+qqa108@users.noreply.github.com>
2024-08-19 15:03:24 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf(".  .   .\n");
+    printf("|  | _ | _. _ ._ _  _\n");
+    printf("|/\\");
+    printf("|(/.|(_.(_)[ | )(/.");
+    
+    return 0;
+}
