d3c1a49d89e10b1476cace70d7f16585b24a8d6e
Choi yonghun <52096623+qqa108@users.noreply.github.com>
2024-08-19 16:11:26 +0900

    [Bronze V] Title: Serca, Time: 1408 ms, Memory: 29316 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=0; i<N; i++) {
+			System.out.println( 
+					" @@@   @@@ \n" + 
+					"@   @ @   @\n" + 
+					"@    @    @\n" + 
+					"@         @\n" + 
+					" @       @ \n" + 
+					"  @     @  \n" + 
+					"   @   @   \n" + 
+					"    @ @    \n" + 
+					"     @     ");
+		}
+	}
+}
