94ac99d28115cb07db2f59ec938a7a1d3b132988
Choi yonghun <52096623+qqa108@users.noreply.github.com>
2024-08-19 14:46:09 +0900

    [Silver III] Title: 이친수, Time: 212 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        long dp[] = new long[num+1];
+
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i=2; i<=num; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        System.out.println(dp[num]);
+
+        scanner.close();
+    }
+}
