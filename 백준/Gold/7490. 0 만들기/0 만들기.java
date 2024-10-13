161e2f31112a6329243376283408f46805dface0
Choi yonghun <52096623+qqa108@users.noreply.github.com>
2024-10-13 21:49:49 +0900

    [Gold V] Title: 0 만들기, Time: 64 ms, Memory: 11464 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,77 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N;
-	static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int tc = Integer.parseInt(st.nextToken());
-		for (int t = 0; t < tc; t++) {
-			st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			dfs(2, "1");
-			sb.append('\n');
-		}
-		System.out.println(sb);
-	}
+        String arr[] = new String[10];
+        arr[3] = "1+2-3\n";
+        arr[4] = "1-2-3+4\n";
+        arr[5] = "1 2-3-4-5\n";
+        arr[6] = "1 2+3-4-5-6\n";
+        arr[7] = "1+2-3+4-5-6+7\n" +
+                "1+2-3-4+5+6-7\n" +
+                "1-2 3+4+5+6+7\n" +
+                "1-2 3-4 5+6 7\n" +
+                "1-2+3+4-5+6-7\n" +
+                "1-2-3-4-5+6+7\n";
+        arr[8] = "1 2-3 4-5 6+7 8\n" +
+                "1+2 3-4 5+6+7+8\n" +
+                "1+2+3+4-5-6-7+8\n" +
+                "1+2+3-4+5-6+7-8\n" +
+                "1+2-3+4+5+6-7-8\n" +
+                "1+2-3-4-5-6+7+8\n" +
+                "1-2 3-4+5+6+7+8\n" +
+                "1-2+3-4-5+6-7+8\n" +
+                "1-2-3+4+5-6-7+8\n" +
+                "1-2-3+4-5+6+7-8\n";
+        arr[9] = "1 2+3 4-5 6-7+8+9\n" +
+                "1 2+3+4-5-6-7+8-9\n" +
+                "1 2+3-4 5+6+7+8+9\n" +
+                "1 2+3-4+5-6+7-8-9\n" +
+                "1 2-3+4+5 6-7 8+9\n" +
+                "1 2-3+4+5+6-7-8-9\n" +
+                "1 2-3-4-5+6-7-8+9\n" +
+                "1 2-3-4-5-6+7+8-9\n" +
+                "1+2-3 4-5 6+7 8+9\n" +
+                "1-2 3-4-5 6-7+8 9\n" +
+                "1-2-3 4+5+6+7+8+9\n";
 
-	static void dfs(int idx, String val) {
-		if (idx == N + 1) {
-			int left = 0;
-			int op = 0;
-			int right = 0;
-
-			for (int i = 0; i < val.length(); i++) {
-				if (val.charAt(i) == '+') {
-					if (right != 0) {
-						left = cal(left, op, right);
-						right = 0;
-					}
-					op = 1;
-				}
-				else if (val.charAt(i) == '-') {
-					if (right != 0) {
-						left = cal(left, op, right);
-						right = 0;
-					}
-					op = -1;
-				}
-				else if (val.charAt(i) == ' ') {
-					continue;
-				}
-				else {
-					if (op == 0) {
-						left = (left * 10) + ((int) val.charAt(i) - '0');
-					}
-					else {
-						right = (right * 10) + ((int) val.charAt(i) - '0');
-					}
-				}
-			}
-			left = cal(left, op, right);
-
-			if (left == 0) {
-				sb.append(val).append('\n');
-			}
-			return;
-		}
-		dfs(idx + 1, val + " " + idx);
-		dfs(idx + 1, val + "+" + idx);
-		dfs(idx + 1, val + "-" + idx);
-	}
-
-	static int cal(int left, int op, int right) {
-		if (op == 1) {
-			left = left + right;
-		}
-		else {
-			left = left - right;
-		}
-		return left;
-	}
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            sb.append(arr[N]).append('\n');
+        }
+        System.out.print(sb);
+    }
 }
