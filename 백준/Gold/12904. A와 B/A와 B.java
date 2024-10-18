a003de7ea9ca68dff1b509a9beb5025d01ba6f89
Choi yonghun <52096623+qqa108@users.noreply.github.com>
2024-10-18 23:01:21 +0900

    [Gold V] Title: A와 B, Time: 80 ms, Memory: 14844 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.java
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -2,49 +2,29 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static String S, T;
-    static int answer;
-    static boolean finished;
 
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        S = st.nextToken();
+        String S = st.nextToken();
         st = new StringTokenizer(br.readLine());
-        T = st.nextToken();
+        String T = st.nextToken();
 
-        answer = 0;
-        finished = false;
-        dfs(S);
-        System.out.println(answer);
-    }
-
-    static void dfs(String idx) {
-        if(T.equals(idx)) {
-            answer =1;
-            finished = true;
-            return;
-        }
-        if (!finished && idx.length() < T.length()) {
-            StringBuilder A = new StringBuilder();
-            StringBuilder B = new StringBuilder();
-            A.append(idx);
-            B.append(idx);
-
-            A.append('A');
-            String strA = A.toString();
-            String revA = (A.reverse()).toString();
-            if (T.contains(strA) || T.contains(revA)) {
-                dfs(strA);
-            }
-
-            B.reverse();
-            B.append('B');
-            String strB = B.toString();
-            String revB = (B.reverse()).toString();
-            if (T.contains(strB) || T.contains(revB)) {
-                dfs(strB);
+        while (T.length() > S.length()) {
+            int idx = T.length() - 1;
+            char ch = T.charAt(idx);
+            if (ch == 'A') {
+                T = T.substring(0, idx);
+            } else if (ch == 'B') {
+                T = T.substring(0, idx);
+                T = new StringBuilder(T).reverse().toString();
             }
         }
+        if (T.equals(S)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
     }
+
 }
